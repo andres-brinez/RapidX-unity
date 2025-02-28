9ea9928cb902d26b46e4085063bf6bd86d0a3daa
Otro Nombre <otrocorreo@ejemplo.com>
2025-02-28 13:47:14 -0500

    Se limpia el código

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -9,19 +9,13 @@ public class Coin : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {
-        
-    }
+    {}
 
     // Update is called once per frame
     void Update()
-    {
-        
-    }
+    {  }
 
     private void OnTriggerEnter(Collider other) {
-
-        Debug.Log("Coin Collected");
 
         if (other.gameObject.tag == "Car")
         {
